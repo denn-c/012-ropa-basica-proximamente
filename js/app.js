d5faa1b9e938fccd0e3bc6f4504939a11231bab5
denn <dennyschuyma20@gmail.com>
2022-06-23 11:53:17 -0500

    first commit

---
+++ js/app.js
@@ -0,0 +1,17 @@
+const $formulario = document.querySelector('.formulario')
+const $correo = document.querySelector('.formulario__correo')
+
+const correo = /^[áéíóúñüa-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[áéíóúñüa-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[áéíóúñüa-z0-9](?:[áéíóúñüa-z0-9-]*[áéíóúñüa-z0-9])?\.)+[áéíóúñüa-z0-9](?:[áéíóúñüa-z0-9-]*[áéíóúñüa-z0-9])?$/
+
+$formulario.addEventListener('submit', e => {
+    e.preventDefault()
+    if (correo.test($correo.value)) {
+        $formulario.submit()
+    } else {
+        $formulario.classList.add('formulario__activo')
+    }
+})
+
+$correo.addEventListener('keyup', e => {
+    $formulario.classList.remove('formulario__activo')
+})
